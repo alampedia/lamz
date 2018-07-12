e27c29f40ef752cbe9b4c7f372fd3ee96648bdba
alampedia <40653142+alampedia@users.noreply.github.com>
2018-07-12 17:06:33 +0700

    Update linkdroid.js

--- linkdroid.js
+++ linkdroid.js
@@ -25,5 +25,5 @@ if(a_to_vk)
 {a_to_vk.innerHTML+=a_to_va;}}
 function a_to_fa()
 {var a_to_vf=new Array();protected_links=protected_links.replace(" ","");a_to_vf=protected_links.split(",");return a_to_vf;}
-protected_links = "blogspot.com,youtube.com,facebook.com,plus.google.com,instagram.com,twitter.com";
+protected_links = "safelinku.com,blogspot.com,youtube.com,facebook.com,plus.google.com,instagram.com,twitter.com";
 auto_safelink();
